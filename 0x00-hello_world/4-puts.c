6d7989b16d3b33cbaded81ffced168fd9d3772cb
Bunmi <ibkdizzu@gmail.com>
2022-06-30 19:00:11 -0700

    run c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
  * Task: compile and print
  */
 #include <stdio.h>
-/* 
- * main: print a string.
- * Return: Always0.
+/**
+ * main: Prints "Programming is like building a multilingual
+ * 		puzzle, followed by a new line
+ * Return: Always 0.
  */
 int main(void)
 {
