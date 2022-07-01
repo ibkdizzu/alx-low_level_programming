5a0953f40fb08b6906478bcbf087730d0ff32831
Bunmi <ibkdizzu@gmail.com>
2022-06-30 18:53:11 -0700

    run c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
+/*
+ * Task: compile and print
+ */
 #include <stdio.h>
+/* main: print a string.
+ * Return: Always0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
