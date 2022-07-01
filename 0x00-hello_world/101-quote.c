27571b619e86566c29f96e790969f51ce272619a
Bunmi <ibkdizzu@gmail.com>
2022-06-30 19:57:03 -0700

    run c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
  * File: 101-quote.c
  * Auth: Bunmi
  */
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Prints "and the piece of art is useful"-Dora Korpar, 2015-10-19",
  * followed by a new line, to dtandard error.
@@ -13,6 +13,6 @@ int main(void)
 {
 	write(2,
 		"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
-		59));
+		59);
 
 	return (1);
