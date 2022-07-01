abf8d312abaf4c086d0719e2ec241b18899b489a
Bunmi <ibkdizzu@gmail.com>
2022-06-30 19:11:12 -0700

    run c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  */
 #include <stdio.h>
 /**
- * main: Prints "Programming is like building a multilingual puzzle"1
+ * main: Prints "Programming is like building a multilingual puzzle"
  * Return: Always 0.
  */
 int main(void)
