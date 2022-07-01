cbdb1171805da1baf9d3d430de793861499dd279
Bunmi <ibkdizzu@gmail.com>
2022-06-30 19:18:20 -0700

    run c task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-printf.c
+ * Auth: Bunmi
+ */
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammer, bu the outcome is
+ * 	a piece of a piece of art," followed by a new line.
+ *
+ * Retun: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	retun (0);
+}
