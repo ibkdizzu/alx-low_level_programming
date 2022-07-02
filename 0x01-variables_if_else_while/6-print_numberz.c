c59c9864cc3c2fa998faccf66826837a07dab31f
Bunmi <ibkdizzu@gmail.com>
2022-07-01 19:49:01 -0700

    run c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits 0 to 9
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 18; i < 28; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
