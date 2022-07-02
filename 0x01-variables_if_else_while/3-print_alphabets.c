cb9b1a6dd98cb40e75443af842c6978fb9481027
Bunmi <ibkdizzu@gmail.com>
2022-07-01 18:49:50 -0700

    run c task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabeths in lower and upper cases.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; c++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; c++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
