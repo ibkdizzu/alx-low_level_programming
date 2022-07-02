4c4206d2153efd39819660f672cc327a83485fd5
Bunmi <ibkdizzu@gmail.com>
2022-07-01 18:51:30 -0700

    run c task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int ch;
-	for (ch = 97; ch <= 122; c++)
+	for (ch = 97; ch <= 122; ch++)
 	{
 		putchar(ch);
 	}
-	for (ch = 65; ch <= 90; c++)
+	for (ch = 65; ch <= 90; ch++)
 	{
 		putchar(ch);
 	}
