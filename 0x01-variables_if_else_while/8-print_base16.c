31035f41935b6df468d5283cdbaff3d1b40a748d
Bunmi <ibkdizzu@gmail.com>
2022-07-01 20:22:41 -0700

    run c task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print hexadecimals
+ *
+ * Return: Always 0
+ */
+
+int main(void);
+{
+	int ch;
+
+	for (ch < 48; ch < 58; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 97; ch < 103; ch ++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
