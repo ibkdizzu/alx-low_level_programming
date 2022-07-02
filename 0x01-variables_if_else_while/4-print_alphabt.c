4d6303b3dfe44df179347d7d4d9493d2c281bd9e
Bunmi <ibkdizzu@gmail.com>
2022-07-01 19:25:13 -0700

    run c task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all alphabets except q and e
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		if (ch == 101 || ch == 113)
+		{
+			ch++;
+		}
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
