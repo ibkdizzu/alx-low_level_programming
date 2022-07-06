67ddcb4c34e229f463e7b9e3004c7d5d61707528
Bunmi <ibkdizzu@gmail.com>
2022-07-06 14:39:22 -0700

    run c task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,8 @@ void times_table(void)
 
 	 for (i = 0; i < 10; i++)
 	 {
+		 for (j = 0; j < 10; j++)
+		 {
 		 k = j * i;
 		 if (j == 0)
 		 {
@@ -33,4 +35,5 @@ void times_table(void)
 		 }
 	 }
 	 _putchar('\n');
+	 }
 }
