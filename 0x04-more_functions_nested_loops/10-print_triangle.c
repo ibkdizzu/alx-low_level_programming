174aeb237a8579f971958728fed5c29729db912b
Bunmi <ibkdizzu@gmail.com>
2022-07-07 18:40:35 -0700

    run task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Draws a triangle
+ * @size: Long of the triangle
+ *
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar(j < (size - i) ? ' ' : '#');
+		}
+
+		_putchar('\n');
+	}
+	if (i == 0)
+		_putchar('\n');
+}
