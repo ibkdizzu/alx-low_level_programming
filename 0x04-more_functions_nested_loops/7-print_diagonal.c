8aca130952277585de19645e38e9fc780f311284
Bunmi <ibkdizzu@gmail.com>
2022-07-07 18:06:41 -0700

    run task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,8 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_purchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -18,7 +20,4 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
-
-	if (i == 0)
-		_putchar('\n');
 }
