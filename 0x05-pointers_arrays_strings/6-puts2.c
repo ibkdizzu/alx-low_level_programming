f0a6e6403ffab32b8af07d90f8043e7a5eb0434a
Bunmi <ibkdizzu@gmail.com>
2022-07-12 18:22:34 -0700

    run c task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a string
+ * @str: string
+ * Return: returns a string
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++
+	}
+}
