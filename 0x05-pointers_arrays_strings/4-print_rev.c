f84eb44de8acb55dea329b897e745375f60a0ace
Bunmi <ibkdizzu@gmail.com>
2022-07-12 21:34:14 -0700

    run c task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,20 @@
 
 void print_rev(char *s)
 {
-	int count = 0;
+	int i, j, len;
 
-	while (count >= 0)
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (s[count]== '\0')
-			break;
-		count++;
+		i++;
 	}
 
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
-	_putchar('\n');
+	len = i;
+
+	for (j = len - 1; j >=0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n);
 }
