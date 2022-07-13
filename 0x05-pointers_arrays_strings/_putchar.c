0fa05ee442662dbb41245ac175a88bb508462971
Bunmi <ibkdizzu@gmail.com>
2022-07-12 21:41:14 -0700

    run c task

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
  * _putchar - writes the charater c to stdout
@@ -10,9 +9,6 @@
  */
 
 int _putchar(char c)
-
 {
-
-		return (write(1, &c, 1));
-
+	return (write(1, &c, 1));
 }
