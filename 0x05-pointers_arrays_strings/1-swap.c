7bebc02244ea723244d786af837e3f4c714c9b3e
Bunmi <ibkdizzu@gmail.com>
2022-07-12 17:13:08 -0700

    run c task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: integer value
+ * @b: integer value
+ * Return: nno value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
