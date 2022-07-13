7247a65e4255c41d2a8ee45c43d95e30dbe839e9
Bunmi <ibkdizzu@gmail.com>
2022-07-12 19:03:47 -0700

    run c task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strcpy - copie string
+ * @dest: a buffer
+ * @src: source string
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
