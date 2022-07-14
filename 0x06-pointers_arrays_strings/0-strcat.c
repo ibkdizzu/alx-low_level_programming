1090853244290a1b289c8b2a550b3b61c6db84f0
Bunmi <ibkdizzu@gmail.com>
2022-07-14 11:43:20 -0700

    run c task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,29 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i, j, len;
 
-	i = j = 0;
+	i = j = len = 0;
 
 	while (dest[i] != '\0')
+	{
 		i++;
-
+	}
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
-		i++;
 	}
 
-	dest[i] = '\0';
+	len = j;
+	j = 0;
+
+	while (j < len)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
 	return (dest);
 }
